2e14602d45d31b24c51c8f867ab51982abbdc56e
github username <github email address>
2020-03-02 05:41:17 -0500

    Will create initial Search component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import Search from './components/stock/Search';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Navbar />
+        <Search />
       </div>
     );
   }
