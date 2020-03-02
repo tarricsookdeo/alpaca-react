7ed868da678a563e31541594a86df8eba00370ce
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-03-02 18:18:51 -0500

    Creates boilerplate for view structure.

---
+++ src/views/Porfolio.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Porfolio extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Porfolio Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Porfolio;
---
+++ src/views/StockInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class StockInfo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Stock Info Page</h1>
+      </div>
+    );
+  }
+}
+
+export default StockInfo;
