3926f359d693944ab531a274889d12318e95e288
github username <github email address>
2020-03-02 05:20:45 -0500

    Creates initial Navbar component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>App Component</h1>
+        <Navbar />
       </div>
     );
   }
