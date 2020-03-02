9895f5296a5d27a48d61c7f1007df7f2030ae786
github username <github email address>
2020-03-02 05:37:54 -0500

    Will create initial Navbar design.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className='bg-blue-900 pt-2 pb-2'>
+      <h1 className='text-white ml-20 text-2xl inline'>Alpaca Trading</h1>
+      <a
+        className='float-right text-white mt-1 mr-20 hover:text-purple-400'
+        href=''
+      >
+        Account
+      </a>
+      <a
+        className='float-right text-white mt-1 mr-10 hover:text-purple-400'
+        href=''
+      >
+        Portfolio
+      </a>
+    </nav>
+  );
 };
 
 export default Navbar;
