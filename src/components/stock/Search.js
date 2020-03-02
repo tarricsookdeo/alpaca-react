17ced548005a546d14dcadba8ac9775167215f20
github username <github email address>
2020-03-02 05:59:52 -0500

    Will create initial Search component design.

--- src/components/stock/Search.js
+++ src/components/stock/Search.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react';
 class Search extends Component {
   render() {
     return (
-      <div>
-        <h1>Search</h1>
-      </div>
+      <form>
+        <input
+          className='w-1/4 mt-3 py-1 pl-3 ml-20 shadow-lg shadow-blue focus:outline-none'
+          type='text'
+          placeholder='Search stock ticker...'
+        />
+        <button
+          className='bg-purple-500 hover:bg-purple-700 shadow-lg text-white ml-2 py-1 px-4 rounded'
+          type='submit'
+        >
+          Search
+        </button>
+      </form>
     );
   }
 }
