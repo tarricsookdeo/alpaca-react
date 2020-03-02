550f61e6adc80f49204d7697314fd9b663438b93
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-03-02 18:31:58 -0500

    Will add react-router-dom to project.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Portfolio from './views/Porfolio';
 import Trade from './views/Trade';
 import StockInfo from './views/StockInfo';
 import Account from './views/Account';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Navbar />
-      </div>
+      <Router>
+        <div className='App'>
+          <Navbar />
+        </div>
+      </Router>
     );
   }
 }
